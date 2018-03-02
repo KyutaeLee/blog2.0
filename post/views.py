c75186ec7a832026079111c77873082c48a630cf
kyutaelee <rbxofjqn@gmail.com>
2018-03-02 18:59:18 +0900

    1day 1commit

--- post/views.py
+++ post/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 def index(request):
     return render(request, 'post/index.html')
+
+class UploadView(CreateView):
+    model = Post
+    fields = ['photo', 'text']
+    template_name = 'post/upload.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['photo', 'text']
+    template_name = 'post/upload.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+
