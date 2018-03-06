8cc1d1f677c3c0058719724e5bc5b19e800ad403
kyutaelee <rbxofjqn@gmail.com>
2018-03-06 21:07:43 +0900

    1day 1commit

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+
 ]
 
--- post/views.py
+++ post/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from .models import Post
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'post/index.html')
+@login_required
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post/list.html', {'posts':posts})
 
-class UploadView(CreateView):
+class UploadView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['photo', 'text']
     template_name = 'post/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else :
+            return self.render_to_response({'form':form})
+
 
 class PostUpdateView(UpdateView):
     model = Post
