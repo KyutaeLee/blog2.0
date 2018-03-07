feb0344f29e2a0354ca5a09438c87b78989f3126
kyutaelee <rbxofjqn@gmail.com>
2018-03-07 21:14:01 +0900

    1day 1commit

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+
+from post.models import Post
 from . import views
+from django.views.generic.detail import DetailView
 
 app_name = 'post'
 
 urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('upload', views.UploadView.as_view(), name='post_create'),
+    path('single/<int:pk>', DetailView.as_view(model=Post, template_name='post/detail.html'), name='post_detail'),
 
 ]
 
