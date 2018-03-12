8eb94d3ef5345db605173c87e56de544c6b55130
kyutaelee <rbxofjqn@gmail.com>
2018-03-12 21:19:27 +0900

    1day 1commit

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,18 @@ from .forms import *
 
 # Create your views here.
 
+def register(request):
+    if request.method == 'POST':
+        user_form = ResisterForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user':new_user})
+    else:
+        user_form = ResisterForm()
+
+    return render(request, 'registration/register.html', {'form':user_form})
+
+
